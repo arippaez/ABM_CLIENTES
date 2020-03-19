7462850182a728d9438f77263b6e4c4ee427a10d
ntarche <nelson.tarche@gmail.com>
2020-03-18 21:55:35 -0300

    Cambios

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors','1');
+ini_set('display_startup_errors','1');
+ini_set('error_reporting', E_ALL); 
+
+
 if(file_exists("clientes.txt")){
     $jsonClientes = file_get_contents("clientes.txt"); 
     $aClientes = json_decode($jsonClientes, true);
@@ -6,19 +12,30 @@ if(file_exists("clientes.txt")){
     $aClientes = array();
 }
 
-$pos = $_GET["pos"];
-print_r($pos);
-exit;
+$pos = isset($_GET["pos"])? $_GET["pos"] : "";
 
-if($_POST){ /* es postback */
+
+if($_POST){ /* es postback, porque alguien hizo clic en guardar */
     //Definicion de variables
     $dni = $_POST["txtDni"];
     $nombre = $_POST["txtNombre"];
     $telefono = $_POST["txtTelefono"];
     $correo = $_POST["txtCorreo"];
 
-    //Analizamos cada accion de los botones
-    if(isset($_POST["btnInsertar"])){
+    if(isset($_GET["do"]) && $_GET["do"] == "edit"){
+        //Modificar la posicion del cliente a editar, cambiando por los nuevos campos
+        $aClientes[$pos] = array("dni" => $dni,
+                            "nombre" => $nombre,
+                            "telefono" => $telefono,
+                            "correo" => $correo);
+
+        //Convertir el array en json
+        $jsonClientes = json_encode($aClientes);
+
+        //Guardar en el archivo la variable json
+        file_put_contents("clientes.txt", $jsonClientes);
+
+    } else {
         //Convertir los datos del formulario en un array
         $aClientes[] = array("dni" => $dni,
                             "nombre" => $nombre,
@@ -30,12 +47,16 @@ if($_POST){ /* es postback */
 
         //Guardar en el archivo la variable json
         file_put_contents("clientes.txt", $jsonClientes);
-    } else if(isset($_POST["btnModificar"])) {
-
-    } else if(isset($_POST["btnBorrar"])) {
-
     }
 }
+
+if(isset($_GET["do"]) && $_GET["do"] == "delete"){
+    unset($aClientes[$pos]);
+    //Guardar en el archivo, el nuevo array de clientes modificado
+    $jsonClientes = json_encode($aClientes);
+    file_put_contents("clientes.txt", $jsonClientes);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -60,32 +81,34 @@ if($_POST){ /* es postback */
                     <div class="row">
                         <div class="col-12 form-group">
                             <label for="txtDni">DNI:</label>
-                            <input type="text" id="txtDni" name="txtDni" class="form-control" required >
+                            <input type="text" id="txtDni" name="txtDni" class="form-control" required value="<?php echo isset($aClientes[$pos]["dni"])? $aClientes[$pos]["dni"] : ""; ?>">
                         </div>
                         <div class="col-12 form-group">
                             <label for="txtNombre">Nombre:</label>
-                            <input type="text" id="txtNombre" name="txtNombre" class="form-control" required>
+                            <input type="text" id="txtNombre" name="txtNombre" class="form-control" required value="<?php echo isset($aClientes[$pos]["nombre"])? $aClientes[$pos]["nombre"] : ""; ?>">
                         </div>
                         <div class="col-12 form-group">
                             <label for="txtTelefono">Teléfono:</label>
-                            <input type="text" id="txtTelefono" name="txtTelefono" class="form-control" required> 
+                            <input type="text" id="txtTelefono" name="txtTelefono" class="form-control" required value="<?php echo isset($aClientes[$pos]["telefono"])? $aClientes[$pos]["telefono"] : ""; ?>"> 
                         </div>
                         <div class="col-12 form-group">
                             <label for="txtCorreo">Correo:</label>
-                            <input type="text" id="txtCorreo" name="txtCorreo" class="form-control" required>
+                            <input type="text" id="txtCorreo" name="txtCorreo" class="form-control" required value="<?php echo isset($aClientes[$pos]["correo"])? $aClientes[$pos]["correo"] : ""; ?>">
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-12">
-                            <button type="submit" id="btnInsertar" name="btnInsertar" class="btn btn-primary">Insertar</button>
-                            <button type="reset" id="btnLimpiar" name="btnLimpiar" class="btn btn-secondary">Limpiar</button>
-                            <button type="submit" id="btnBorrar" name="btnBorrar" class="btn btn-danger">Borrar</button>
-                            <button type="submit" id="btnModificar" name="btnModificar" class="btn btn-success">Modificar</button>
+                            <button type="submit" id="btnInsertar" name="btnInsertar" class="btn btn-primary">Guardar</button>
                         </div>
                     </div>
                 </form>
             </div>
             <div class="col-6">
+                <div class="row">
+                    <div class="col-12">
+                        <a href="index.php?do=new"><i class="fas fa-user-plus"></i></a>
+                    </div>
+                </div>
                 <table class="table table-hover border">
                     <tr>
                         <th>DNI</th>
@@ -93,14 +116,22 @@ if($_POST){ /* es postback */
                         <th>Correo</th>
                         <th>Acciones</th>
                     </tr>
-                    <?php foreach ($aClientes as $cliente) { ?>
+                    <?php 
+                    foreach ($aClientes as $pos => $cliente) {
+                           ?>
+                 
                     <tr>
                         <td><?php echo $cliente["dni"]; ?></td>
                         <td><?php echo $cliente["nombre"]; ?></td>
                         <td><?php echo $cliente["correo"]; ?></td>
-                        <td><i class="fas fa-edit"></i></td>
+                        <td>
+                            <a href="?pos=<?php echo $pos; ?>&do=edit"><i class="fas fa-edit"></i></a>
+                            <a href="?pos=<?php echo $pos;?>&do=delete"><i class="fas fa-trash-alt"></i></a>
+                        
+                        </td>
                     </tr>
-                    <?php } ?>
+                    <?php 
+                    } ?>
                 </table>
             </div>
         </div>
